e0735b3b9b732114485bb133f1edcd896c804d1f
cejolietruc <nanaunina@gmail.com>
2026-04-04 15:29:23 +0200

    merge audio file

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
+import re
 from pathlib import Path
 from flask import Flask, render_template, request, url_for, flash
 from google.cloud import texttospeech
+from google.api_core.exceptions import GoogleAPIError
 import fitz
 import os
-
-from werkzeug.utils import redirect
+from moviepy import AudioFileClip, concatenate_audioclips, AudioClip
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['appkey']
 
-directory = os.path.join(app.static_folder, "files")
-
 @app.route('/', methods=['POST','GET'])
 def home():
-
+    directory = os.path.join(app.static_folder, "files")
+    audio_list = []
     if request.method == 'POST':
         language = request.form.get("language")
         file = request.files["file"]
@@ -27,7 +27,7 @@ def home():
 
         # # ---------- READ PDF ----------
         doc = fitz.open(f"./static/files/{file.filename}")
-        file_name = file.filename.strip(".pdf")
+        file_name = Path(file.filename).stem
         print("Number of pages:", len(doc))
 
         all_text = {}
@@ -41,6 +41,14 @@ def home():
 
         print("Pages extracted:", len(all_text))
         print(all_text)
+
+        # ---------- MAKE A FOLDER -------
+
+        filepath = Path(f"./static/files/{file_name}")
+        folder_name = filepath.stem
+        print(folder_name)
+        output_dir = Path(app.static_folder) / "files" / folder_name
+        output_dir.mkdir(parents=True, exist_ok=True)
 
         # ---------- GOOGLE TTS ----------
         for page_no in all_text:
@@ -66,35 +74,59 @@ def home():
 
             )
 
-            response = client.synthesize_speech(
-                input=synthesis_input,
-                voice=voice,
-                audio_config=audio_config
-            )
-            # except:
-            #     flash("API requests failed...")
-            #     print("API requests failed...")
+            try:
+                response = client.synthesize_speech(
+                    input=synthesis_input,
+                    voice=voice,
+                    audio_config=audio_config
+                )
+            except GoogleAPIError(Exception) as e:
+                print("FULL ERROR:", repr(e))
+                flash("API requests failed...")
+                print("API requests failed...")
+            else:
+                # ---------- SAVE AUDIO ----------
+                with open(f"{output_dir}/audio_{page_no}.mp3", "wb") as out:
+                    out.write(response.audio_content)
 
-            # ---------- MAKE A FOLDER -------
+                print(f"Audio saved to {output_dir}/audio_{page_no}.mp3")
+                new_audio = output_dir/f"audio_{page_no}.mp3"
+                audio_list.append(new_audio)
 
-            filepath = Path(f"./static/files/{file_name}")
-            folder_name = filepath.stem
-            print(folder_name)
-            output_dir = Path(app.static_folder) / "files" / folder_name
-            output_dir.mkdir(parents=True, exist_ok=True)
+        # --------- MERGE AUDIO FILES ------
+        # list of mp3 files
+        audio_sorted = sorted(audio_list, key=lambda f: int(re.findall(r'\d+', f.stem)[0]))
+        # re.findall() searches a string for all matches of a pattern and returns a list of matches
+        print(audio_sorted)
 
-            # ---------- SAVE AUDIO ----------
-            with open(f"./static/files/{folder_name}/{file_name}_{language}_page_{page_no}.mp3", "wb") as out:
-                out.write(response.audio_content)
+        # Create 1-second silent clip (duration=1)
+        silence = AudioClip(lambda t: 0, duration=1, fps=44100)
 
-            print(f"Audio is successfully saved")
+        # Load audio clips
+        audio_files = [AudioFileClip(audio) for audio in audio_sorted]
+        print(audio_files)
+        audio_silence_list = []
+        for file in audio_files:
+            audio_silence_list.append(file)
+            audio_silence_list.append(silence)
+
+        # Concatenate all clips
+        final_audio = concatenate_audioclips(audio_silence_list)
+
+        # Export merged audio
+        audio_filename = f"{file_name}_{language}.mp3"
+        audio_path = Path(output_dir) / audio_filename
+        final_audio.write_audiofile(str(audio_path))
+
+        # --------- SHOW AUDIO FILES ------
+        if audio_path is None:
+            message = "No Audio file(s) found"
+
+        else:
             message = "Audio file(s) ready for download :"
 
-            # --------- SHOW AUDIO FILES ------
-            files = os.listdir(output_dir)
-            #files = [f for f in files if f.endswith(".pdf")]
 
-            return render_template("index.html", files=files, folder=folder_name, message=message)
+        return render_template("index.html", file=audio_filename, folder=folder_name, message=message)
 
     return render_template("index.html")
 
